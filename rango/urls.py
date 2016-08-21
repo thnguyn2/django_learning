2e6400f1035b3c941653073821049393251cda6f
Tan Nguyen <thnguyn2@illinois.edu>
2016-08-21 18:14:12 -0500

    Exercise of Chapter 3 completed

--- rango/urls.py
+++ rango/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^$', views.index2, name='index 2 function')
+    url(r'^$', views.index2, name='index 2 function'),
+    url(r'^about/$', views.about, name='about function'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, this is the first response from the Rango view.")
+    return HttpResponse("Hello, this is the first response from the Rango view. See the about page <a href='/rango/about'>About</a>")
 
 def index2(request):
     return HttpResponse("Hello, this is the second response from the Rango view.")
+
+def about(request):
+    return HttpResponse("Rango says here is the about page. Go back to the <a href='/'>main</a> page. Otherwise, go to the <a href='/rango/''>second</a> page")
