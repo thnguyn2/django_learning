b865f21b783ff6443336b935b8b7102be58b3f74
Tan Nguyen <thnguyn2@illinois.edu>
2016-08-30 09:22:13 -0400

    Done with section 8.3

--- rango/urls.py
+++ rango/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 #This
 import views
+app_name = 'rango'
 
 urlpatterns = [
     url(r'^$', views.index2, name='index 2 function'),
-    url(r'^about/$', views.about, name='about function'),
-    url(r'^add_category/$', views.add_category,name='add_category'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
     #<category_name_slug> is a variable name, allows us to extract the value later on, [\w\-]+ is any alphanumeric sequence
